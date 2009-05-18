79b43db5693048f24d26151c3d791a079251b31f
netshade <netshade@gmail.com>
2009-05-18 15:49:45 -0400

    reformat docs

--- lib/sql_valued_columns.rb
+++ lib/sql_valued_columns.rb
@@ -5,15 +5,29 @@ module SqlValuedColumns
     base.alias_method_chain(:attributes_with_quotes, :sqlcolumns)    
   end
 
+  # Includes the SqlValuedColumns module to ActiveRecord, overriding existing value insertion functionality.
   def self.activate!
     ActiveRecord::Base.class_eval { include ::SqlValuedColumns }
   end
 
   module ClassMethods
-    
+
+    # Indicates the given column will have its value filled by the result of an SQL function.  Pass a hash as the last 
+    # argument with any optional args.
+    #
+    # ==== Arguments
+    # * <tt>column</tt> - +Symbol+, the name of the column to fill
+    # * <tt>function</tt> - +String+, the name of the function to execute, or the literal SQL to call if options[:raw] is true
+    # * <tt>args</tt> - +Array+ of arguments to pass to the SQL function specified.  If an argument is a +Symbol+, the name it 
+    #   represents will be called on the model at save time.  If it is a +Proc+, it will be called with the
+    #   model passed as the only argument to the +Proc+.  If it is a +String+, the +String+ will evaluated if
+    #   the pattern #{some_code} appears within it.  Once all these conditions have been exhausted, the
+    #   resultant values will be quoted as appropriate by your database adapter and populate the arguments
+    #   list of the SQL function you specified.
+    # * <tt>options</tt> (optional) - Hash of optional arguments ( :raw ) .
     def sql_column(column, function, *args)
       options = args.extract_options!
-      raw = options[:raw] || false
+      raw = options[:raw] == true
       @sql_columns ||= { }
       @sql_columns[column] = [function, args, raw]
     end
@@ -27,17 +41,9 @@ module SqlValuedColumns
   module InstanceMethods
     
     def attributes_with_quotes_with_sqlcolumns(include_primary_key = true, include_readonly_attributes = true, attribute_names = @attributes.keys)
-      quoted = {}
-      connection = self.class.connection
-      attribute_names.each do |name|
-        if (column = column_for_attribute(name)) && (include_primary_key || !column.primary)
-          value = read_attribute(name)
-
-          # We need explicit to_yaml because quote() does not properly convert Time/Date fields to YAML.
-          if value && self.class.serialized_attributes.has_key?(name) && (value.acts_like?(:date) || value.acts_like?(:time))
-            value = value.to_yaml
-          end
-
+      h = self.attributes_with_quotes_without_sqlcolumns(include_primary_key, include_readonly_attributes, attribute_names)
+      names = h.keys
+      names.each do |name|
           if self.class.sql_columns[name]
             function, args, raw = self.class.sql_columns[name]
             unless raw 
@@ -57,17 +63,13 @@ module SqlValuedColumns
                   connection.quote(a)
                 end
               end
-              quoted[name] = "#{function}(#{fmt_args.join(",")})"
+              h[name] = "#{function}(#{fmt_args.join(",")})"
             else
-              quoted[name] = function
+              h[name] = function
             end
-          else
-            quoted[name] = connection.quote(value, column)            
           end
-
-        end
       end
-      include_readonly_attributes ? quoted : remove_readonly_attributes(quoted)
+      h
     end
     
   end
